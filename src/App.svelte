928d8b215bf6309d99bcf7fb54a654748322266b
Mr. Mylad <mrmylad10@gmail.com>
2023-05-31 10:46:08 +0530

    Initialized project

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  // Firebase config
+  import { initializeApp } from "firebase/app";
+  import firebaseConfig from "./firebaseConfig";
+  
+  const app = initializeApp(firebaseConfig);
+</script>
