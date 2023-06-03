c26e099e7ecf59a63b3d3224f69cf00b4cd20e8a
Mr. Mylad <mrmylad10@gmail.com>
2023-06-03 17:07:43 +0530

    Added a homepage, and a way to switch between login and signup

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,9 @@
   
   // Real code
 
+  // Importing stuff
+  import Home from "./Home.svelte";
+
   import { onMount } from "svelte";
 
   // Smooth scroll
@@ -16,7 +19,30 @@
     setTimeout(() => {
       smoothLoadAnimationAdder("animated");
     }, 0);
-  })
+  });
+
+  // Go to Login / Signup code
+  let currentlyAt: string;
+  const GoToLogin: () => void = () => {
+    document.querySelectorAll(".about").forEach(aboutElement => aboutElement.remove());
+    currentlyAt = "login";
+  }
+  
+  const GoToSignup: () => void = () => {
+    document.querySelectorAll(".about").forEach(aboutElement => aboutElement.remove());
+    currentlyAt = "signup";
+  }
 </script>
 
 <h1 class="animated">Welcome to DigitalNotebook!</h1>
+<br class="about"><br class="about"><br class="about"><br class="about"><br class="about">
+
+<Home elementClass="animated about" />
+<br>
+{#if currentlyAt === "login"}
+  <p>Logging in</p>
+{:else if currentlyAt === "signup"}
+  <p>Signing up</p>
+{/if}
+<button class="animated" style="transition-delay: 8s;" on:click={GoToSignup}>Sign up</button>
+<button class="animated" style="transition-delay: 8s;" on:click={GoToLogin}>Log in</button>
---
+++ src/Home.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let elementClass: string;
+</script>
+<h3 class={elementClass} style="transition-delay: 2s;">Ever wanted to have your own personal book?</h3>
+<h3 class={elementClass} style="transition-delay: 3s;">Some place where you could... Write anything you wanted online?</h3>
+<h3 class={elementClass} style="transition-delay: 4s;">Well, in that case, DigitalNotebook is for you!</h3>
+<h3 class={elementClass} style="transition-delay: 5s;">Write anything you want, anywhere you need.</h3>
+<p class={elementClass} style="transition-delay: 6s;">*The only requirements is a computer with a web browser and internet connection.</p>
+<h1 class={elementClass} style="transition-delay: 7s;">Get writing on DigitalNotebook now!</h1>
